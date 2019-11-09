321aef9367c0abad044b744cc7f7a65efd016c00
emilylin-itp.github.io <el3015@nyu.edu>
2019-11-08 22:48:48 -0500

    reorg

---
+++ wk5-7-textarchive/language_dep_final/toggle_menu_author.js
@@ -0,0 +1,39 @@
+let aboutBar;
+let sideMenu;
+let titelContainer;
+
+function init() {
+    aboutBar = document.getElementById("about-container");
+    aboutBar.addEventListener('click', toggleMenu);
+    console.log('about bar clicked!');
+}
+
+
+function toggleMenu() {
+    aboutBar = document.getElementById("about-container");
+    sideMenu = document.getElementById("side-menu-container");
+    titleContainer = document.getElementById("title-container");
+   
+    if (sideMenu.style.visibility === "hidden") {
+        sideMenu.style.visibility = "visible";
+        aboutBar.style.backgroundColor = '#2e2e2e';
+        aboutBar.style.zIndex = '4';
+        document.body.style.overflowY = 'hidden';
+        console.log('show menu');
+    } else {
+        sideMenu.style.visibility = "hidden";
+        console.log('hide menu');
+        aboutBar.style.backgroundColor = 'black';
+        document.body.style.overflowY = 'auto';
+    }
+
+    // if (titleContainer.style.display === 'block') {
+    //     titleContainer.style.display = 'none';
+    // } else {
+    //     titleContainer.style.display = 'block';
+    // }
+}
+
+
+////LOAD////
+window.addEventListener('load', init);
